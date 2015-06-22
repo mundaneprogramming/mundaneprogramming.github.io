34217ff5ead3949be60de6ef6a474fa6019121e1
Dan Nguyen <dansonguyen@gmail.com>
2015-06-22 15:11:13 -0700

    moved to boto3

--- code/stowy.py
+++ code/stowy.py
@@ -1,70 +1,91 @@
-import boto3
 from os import environ
 from os.path import expanduser, basename
 from posixpath import join as posixpathjoin
-from boto.s3.connection import OrdinaryCallingFormat
-from boto.s3.key import Key as S3Key
 from urllib.parse import urlsplit
-import boto.s3
+import boto3
+import argparse
+import requests
+from io import BytesIO
 
 CONN_DEFS = {
     'aws_access_key_id': environ.get('AWS_ACCESS_KEY_ID'),
     'aws_secret_access_key': environ.get('AWS_SECRET_ACCESS_KEY'),
-     'calling_format': OrdinaryCallingFormat(),
+     # 'calling_format': OrdinaryCallingFormat(),
      'region_name': 'us-east-1'
-}
-
-OPT_DEFS = {
-    'bucket_name': 'www.mundaneprogramming.com',
-    'prepend_path': ''
 }
 
 # http://boto.readthedocs.org/en/latest/ref/s3.html
 # http://boto.readthedocs.org/en/latest/s3_tut.html
-
 # https://github.com/boto/boto/issues/2836#issuecomment-77612836
-
-
 def get_file_bytes(path):
     # check to see if remote URL
     if 'http' in urlsplit(path).scheme:
-        f = requests.get(BytesIO(resp.content))
+        f = BytesIO(requests.get(path).content)
     else:
         f = open(path, 'rb')
     return f
 
 
-def get_s3_bucket(conn, bucket_name):
-    bucket = conn.create_bucket(bucket_name)
-    return bucket
+def get_s3_bucket(client, bucket_name, acl = 'public-read'):
+    return client.create_bucket(Bucket = bucket_name, ACL = acl)
 
-def get_s3_conn(aws_access_key_id, aws_secret_access_key, calling_format, region_name):
-    conn = connect_to_region(region_name = region_name,
-                    aws_access_key_id=aws_access_key_id,
-                    aws_secret_access_key=aws_secret_access_key,
-                    calling_format=calling_format)
-    return conn
 
-def upload_single_file(file, bucket, keyname):
-    # uploads the file without concern of an existing key
-    # returns s3.key object
-    thekey = S3Key(bucket)
-    thekey.key = keyname
-    thekey.set_contents_from_file(file)
-    return thekey
 
-# def stowy(path_to_stow, bucket_name = OPT_DEFS['bucket_name'],
-#               prepend_path = OPT_DEFS['start_path'],
-#               conn = get_s3_conn(**CONN_DEFS),
-#               use_basename = True
-#             ):
-def stowy(path_to_stow, bucket_name, conn, prepend_path = "", use_basename = True):
-    bucket = get_s3_bucket(conn, bucket_name)
-    rfname = basename(path_to_stow) if use_basename else path_to_stow
-    keyname = posixpathjoin(prepend_path, rfname)
-    ## todo make better
-    atts = {'bucket': bucket, 'keyname': keyname}
-    atts['file'] = get_file_bytes(path_to_stow)
-    resp = upload_single_file(**atts)
+
+# def get_s3_key(bucket, key_name):
+#     key = S3Key(bucket)
+#     key.name = key_name
+#     return key
+
+
+def get_s3_client(aws_access_key_id, aws_secret_access_key, region_name):
+    session = boto3.Session(aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key,
+                  region_name=region_name)
+    # client = boto.s3.clientect_to_region(region_name = region_name,
+    #                 aws_access_key_id = aws_access_key_id,
+    #                 aws_secret_access_key = aws_secret_access_key,
+    #                 calling_format = calling_format)
+    client = session.client('s3')
+    return client
+
+def upload_single_file(file, client, bucket, key, acl = 'private'):
+    # dumb upload, expects all work to be done in the key
+    resp = client.put_object(ACL = acl, Bucket = bucket, Body = file, Key = key)
     return resp
+
+def stowy(path_to_stow,  client, bucket_name, key_name = None, prepend_path = "", use_basename = True):
+    bucketresp = get_s3_bucket(client, bucket_name)
+    # TODO: check bucketresp
+    bucket = bucket_name
+    if not key_name:
+        key_name = basename(path_to_stow) if use_basename else path_to_stow
+    key = posixpathjoin(prepend_path, key_name)
+    f = get_file_bytes(path_to_stow)
+    resp = upload_single_file(file = f, client = client, bucket = bucket,
+        key = key_name, acl = 'public-read')
+    return resp
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', '-f', nargs = 1,
+        help ='Specify a local filename or URL pointing to a file')
+    parser.add_argument('--bucket', '-b', nargs = 1,
+        help ='Specify a bucket')
+
+    args = parser.parse_args()
+    a_filename = args.filename[0]
+    a_bucketname = args.bucket[0]
+    client = get_s3_client(**CONN_DEFS)
+    # send file
+    resp = stowy(path_to_stow = a_filename, client = client, bucket_name = a_bucketname)
+    print(resp)
+    # TODO: extract url
+    # TODO: option to set limited expire
+    # public_url = key.generate_url(expires_in = 0, query_auth = False, force_http = True)
+    # print(public_url)
+    # # TODO: option to set compression
+    #  - Deal with local directories
+    #  - Deal with wget site mirroring
 
