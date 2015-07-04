4f93b7881f043b6a25ae79ced7bae5ecb5cf79d9
Dan Nguyen <dansonguyen@gmail.com>
2015-07-04 00:26:27 -0500

    allowed CLI use

--- code/casper_capture.js
+++ code/casper_capture.js
@@ -1,8 +1,23 @@
-var casper = require('casper').create();
-// proof of concept
-casper.start('http://ogesdw.dol.gov/homePage.php', function(){
-  this.echo(this.getTitle());
-  this.capture("/tmp/ogesw.jpg");
-});
+// sample URL http://ogesdw.dol.gov/homePage.php
 
+
+function safeurltofilename(txt){
+  var s = txt.split("//").slice(1).join('__');
+  return s.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+}
+
+
+var casper = require('casper').create();
+var url = casper.cli.args[0]
+if( casper.cli.has("output")) {
+  var fname = casper.cli.get("output")
+} else{
+  var fname = safeurltofilename(url) + '.' + new Date().toISOString() + '.jpg'
+}
+
+casper.start(url, function(){
+  this.echo("Capturing " + url + ", title: " + this.getTitle());
+  this.capture(fname)
+  this.echo("Saving to " + fname);
+})
 casper.run();
