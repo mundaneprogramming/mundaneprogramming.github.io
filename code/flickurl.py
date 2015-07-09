a45c3cd1b7894d7d394ab0d62a0d4665a183d87c
Dan Nguyen <dansonguyen@gmail.com>
2015-07-09 10:39:41 -0700

    more data

--- code/flickurl.py
+++ code/flickurl.py
@@ -8,6 +8,7 @@ from os import environ
 import argparse
 import re
 import requests
+import json
 
 BASE_URL = 'https://api.flickr.com/services/rest/'
 
@@ -82,18 +83,32 @@ if __name__ == '__main__':
     photo_id = extract_photo_id(url)
     photo_url_meta = get_biggest_photo_url_meta(photo_id, api_key)
     photo_info = get_photo_info(photo_id, api_key)
+    # extract some variables
+    title = photo_info['title']['_content']
+    canonurl = extract_canonical_url(photo_info)
+    owner_name = photo_info['owner']['realname']
+    source_url = photo_url_meta['source']
+    # We need to sanitize quotemarks before inserting it into HTML
+    title_owner = (title + " by: " + owner_name).replace('"', "''")
+    # print copyable HTML
+    htmllink = """<a href="{page_url}" title="{title}"><img src="{src_url}" alt="{title}"></a>""".format(
+                page_url = canonurl, src_url = source_url, title = title_owner
+            )
+    cprint("HTML image link")
+    print(htmllink)
     # print title
     cprint("Title")
-    print(photo_info['title']['_content'])
+    print(title)
     # print canonical url
     cprint("Photo page")
-    print(extract_canonical_url(photo_info))
-    # print direct url to photo
-    cprint("Direct URL")
-    print(photo_url_meta['source'])
+    print(canonurl)
     # owner name
     cprint("Owner name")
-    print(photo_info['owner']['realname'])
+    print(owner_name)
+
+    # print direct url to photo
+    cprint("Direct Image URL")
+    print(source_url)
     # dates
     if photo_info['dates'].get('taken'):
         cprint("Date taken")
--- examples/flickr-scrape.md
+++ examples/flickr-scrape.md
@@ -3,7 +3,19 @@ title: Extract photo and metadata from Flickr
 ---
 
 
+![image](/files/images/photos/brooklyn4th.jpg)
+
+
+The flickr page:
+
+![image](/files/images/screenshots/flickr.brooklyn4th.jpg){:.b}
+
+
 via explorer: 
 https://www.flickr.com/services/api/explore/flickr.photos.getInfo
 
 https://www.flickr.com/photos/statelibraryofnsw/4944459226/
+
+After the scrape
+
+![image](/files/images/screenshots/flickrscrapeoutput.png)
