6e561e4170ac2272be783e3e7fe43590662ad62c
Dan Nguyen <dansonguyen@gmail.com>
2015-08-11 20:02:40 -0700

    added erorr handling

--- code/gettwitterpeeps_cli.py
+++ code/gettwitterpeeps_cli.py
@@ -8,40 +8,27 @@ import json
 DEFAULT_TWITTER_CREDS_PATH = '~/.creds/me.json'
 BATCH_SIZE = 100
 
-def get_api():
-    fn = os.path.expanduser(DEFAULT_TWITTER_CREDS_PATH)
+def get_api(creds_path):
+    fn = creds_path
     c = json.load(open(fn))
     auth = tweepy.OAuthHandler(consumer_key = c['consumer_key'],
                                consumer_secret = c['consumer_secret'])
     auth.set_access_token(c['access_token'], c['access_token_secret'])
+    # The wait_on_rate_limit parameters let the API handler sleep
+    #   upon hitting a wait limit, and then to automatically continue
+    api = tweepy.API(auth,
+            wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
     return tweepy.API(auth)
 
-def get_5000_friends_ids(screen_name):
-    api = get_api()
-    return api.friends_ids(screen_name, count = 5000)
 
-# breaking the DRY rule
-def get_5000_followers_ids(screen_name):
-    api = get_api()
-    return api.followers_ids(screen_name, count = 5000)
-
-# gets a whole bunch of profile information from a batch of user IDs
-# We don't use followers/list or friends/list because of the stricter API limits
+# This program gets: ids of the given user's friends or followers
+#    then: it uses users/lookup to look up those names in batches
+# We use followers/ids and friends/ids instead of
+#    followers/list or friends/list because of the stricter API limits
+#    in the latter API calls
 # https://dev.twitter.com/rest/reference/get/followers/list
-# compared to followers/ids and users/lookup
 # https://dev.twitter.com/rest/reference/get/followers/ids
 # https://dev.twitter.com/rest/reference/get/users/lookup
-def get_profiles_from_ids(user_ids):
-    api = get_api()
-    batch_size = 100
-    profiles = []
-    for x in range(0, len(user_ids), BATCH_SIZE):
-        print("Batch:", x)
-        batch_uids = user_ids[x:x+BATCH_SIZE]
-        for user in api.lookup_users(batch_uids):
-            profiles.append(user._json)
-
-    return profiles
 
 
 
@@ -49,22 +36,61 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("screen_name", nargs = 1,
         help = "Screen name of user to get tweets from")
-    parser.add_argument("peeps_type", nargs = 1,
+    parser.add_argument("users_type", nargs = 1,
         help = "'friends' or 'followers' to collect")
     parser.add_argument("--output-path", '-o', help = "Path to save to")
+    parser.add_argument("--creds-path", '-c', help = "Path to a JSON file that has Twitter creds",
+            default = DEFAULT_TWITTER_CREDS_PATH
+        )
     # Get screen name from commandline
     args = parser.parse_args()
     screen_name = args.screen_name[0]
-    peeps_type = args.peeps_type[0]
-
+    users_type = args.users_type[0]
+    creds_path = os.path.expanduser(args.creds_path)
+    # talk to the API
+    api = get_api(creds_path)
     # Start the downloading process
-    if peeps_type == 'friends':
-        userids = get_5000_friends_ids(screen_name)
-    elif peeps_type == 'followers':
-        userids = get_5000_followers_ids(screen_name)
+    if users_type == 'friends':
+        foo = api.friends_ids
+    elif users_type == 'followers':
+        foo = api.followers_ids
+    else:
+        raise Exception("users_type must be `friends` or `followers`")
+    user_ids = []
+    cursor = tweepy.Cursor(foo, screen_name = screen_name).pages()
+    while True:
+        try:
+            page = cursor.next()
+            user_ids.extend(page)
+        except StopIteration:
+            break
+        except tweepy.error.TweepError as e:
+            print(e)
+            break
+        else:
+            print("Collected", len(user_ids), " IDs")
+
     # too lazy to catch invalid input here, meh
     # now get the profiles
-    profiles = get_profiles_from_ids(userids)
+    profiles = []
+    for x in range(0, len(user_ids), BATCH_SIZE):
+        batch_uids = user_ids[x:x+BATCH_SIZE]
+        try:
+            results = api.lookup_users(batch_uids)
+
+        # catch situation in which none of the names in the batch are found
+        # or else Tweepy will error out
+        except tweepy.error.TweepError as e:
+            if e.response.status_code == 404:
+                pass
+            else:
+                # some other error, print the exception and
+                # save what we can
+                print(e)
+
+        else:
+            profiles.extend([user._json for user in results])
+            print("User profiles collected:", len(profiles))
 
     # Now save the file
     fn = args.output_path if args.output_path else "%s.%s.json" % (screen_name, peeps_type)
