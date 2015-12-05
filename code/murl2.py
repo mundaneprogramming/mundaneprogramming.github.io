5f35e7d8e2599af51647fab53139af13d7e8c1e0
Dan Nguyen <dansonguyen@gmail.com>
2015-12-04 22:45:02 -0800

    ah need a bettah meta parsa

---
+++ code/murl2.py
@@ -0,0 +1,37 @@
+"""
+Attempts to extract the best versions of:
+
+url:
+title:
+description:
+image_url:
+created_at:
+updated_at:
+publisher_name:
+domain:
+author:
+excerpt:
+
+"""
+
+
+def twitter_card_parser(html):
+    pass
+
+
+def og_tags_parser(html):
+    pass
+
+def standard_meta_parser(html):
+    pass
+
+
+def body_seo_parser(html):
+    pass
+
+
+def url_parser(url):
+    pass
+
+
+
